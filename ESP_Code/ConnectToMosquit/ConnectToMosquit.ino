74d35f544c4d4b14164a11b21872c45c22dd44e4
Daniel Lengyel <dlengyel@salzburgresearch.at>
2017-07-06 15:37:18 +0200

    Updated the esp script. Now it has a buffer which works for one element. Wasn't able to test for more elements yet, even though it should work given that the one worked. I am implementing the json formating myself, since when I tried the library it cost me on average 2ms in reading data. There are some safty feartures which should be built in, like checking callocs and reallocs.

--- ESP_Code/ConnectToMosquit/ConnectToMosquit.ino
+++ ESP_Code/ConnectToMosquit/ConnectToMosquit.ino
@@ -18,27 +18,34 @@ PubSubClient client(espClient);
 //the i2c address found
 const int i2c_addr = 104;
 
-//the counter to know how many data points to send for calibration
-int cal_counter = 0;
-//the number of data points to send for calibration
-int num_cal = 20;
-
 //the last time i sent the message
-int* last_time;
+int begin_time;
 
+char* data; //the data to be sent as a string in json format
+
+//a buffer which will be sent also. mainly in case the connection fails for a second.
+char* data_buffer;
+int size_data_buffer;
+
+
+int data_index; //where the data should be stored in the buffer. 
 void setup() {
-  //serial will not work with i2c given the way i am implementing it
-  //Serial.begin(9600); 
-  cal_counter = 0;
-  last_time = (int *)malloc(3 * sizeof(int));
+  data = (char*)calloc(250, sizeof(char));
+  data_buffer = (char*)calloc(500, sizeof(char));
+  size_data_buffer = 500;
+  data_index = 0;
   
+  //serial will not work with i2c given the way i am implementing it
+  //Serial.begin(9600);
+  begin_time = millis();
+
   //connect to WiFi
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
-  
+
   //need to initialize the pins to be used for i2c. Refer to www.instructables.conm/id/How-to-use-the-ESP8266-01-pins/
   Wire.begin(1, 3);
 
@@ -54,77 +61,76 @@ void setup() {
 
 //connect with mqtt server as ESP8266 client
 void reconnect_mqtt() {
-  //loop until conntected
   while (!client.connected()) {
     //attempt to connect
-    if (client.connect("ESP8266 client")){  
+      read_format_data(); //in case connection breaks it will still add to the buffer. 
+    if (client.connect("ESP8266 client")) {
       
     } else {
-      delay(5000); //wait to reconnect
+      delay(5000); //should implement a mechanism so it stores the data offline in a buffer and then sends the data. 
     }
   }
 }
 
 
 void loop() {
-  if (!client.connected()) {
-    reconnect_mqtt();
-  }
-  client.loop();
-  //to determine which i2c address the accelerometer has
-  //  for(int addr = 1; addr < 127; addr++){
-  //    Wire.beginTransmission(addr);
-  //    if(!Wire.endTransmission()){
-  //      char msg[5];
-  //      sprintf(msg, "%d", addr);
-  //      client.publish(topic, msg);
-  //    }
-  //  }
 
+  read_format_data();
+    if (!client.connected()) {
+    reconnect_mqtt();
+   client.loop(); //This should be called regularly to allow the client to process incoming messages and maintain its connection to the server.
+  } else {
+      sprintf((data_buffer+data_index-1), "]");
+      client.loop(); //This should be called regularly to allow the client to process incoming messages and maintain its connection to the server.
+      client.publish(topic, data_buffer);
+      data_index = 0;
+
+  }
+
+
+}
+// reading the data into a predifined and allocated array. makes more sense from an efficiency perspective. If needed, this data can still be used and put in an array.
+//AKS WETHER IT IS OK TO JUST MODIFY GLOBAL VARIABLES AND NOT TO RETURN ANYTHING.
+void read_format_data() {
   // get mpu data
   Wire.beginTransmission(i2c_addr);
-  Wire.write(0x3B); //starting with register 0x3B(ACCEL_XOUT_H)
+  Wire.write(0x3B); //starting with register 0x3B(ACCEL_XOUT_H). Any read will happen from here on. https://www.invensense.com/wp-content/uploads/2015/02/MPU-6000-Register-Map1.pdf check the map to see which registers might be needed
   Wire.endTransmission(false);
-  Wire.requestFrom(i2c_addr, 6, true);
+  Wire.requestFrom(i2c_addr, 14, true);
   int32_t AcX = (int32_t)(int16_t)(Wire.read() << 8 | Wire.read()); //look at data sheet. Adding the first and last byte accordingly. looking after two's complement
   int32_t AcY = (int32_t)(int16_t)(Wire.read() << 8 | Wire.read());
   int32_t AcZ = (int32_t)(int16_t)(Wire.read() << 8 | Wire.read());
-  
-  int acc_data[] = {AcX, AcY, AcZ};
-  //start calibration process
-  if (cal_counter == 0) {
-    client.publish(topic, "0/Place the object flat on a surface. Calibration will start in 5 seconds."); //0 indicates the warning that calibration will start
-    char msg_num_cal[50];
-    sprintf(msg_num_cal, "1/%d", num_cal);
-    client.publish(topic, msg_num_cal); //1 indicates how many calibration cycles we will have
-    delay(5000);
-    client.publish(topic, "2/start calibration"); //2 indicates the start of calibration
-  } else if (cal_counter == num_cal) {
-    client.publish(topic, "3/end calibration"); //3 indicates end of calibration
-    client.publish(topic, "4/start data transfer"); //4 indicates start of normal data transfer
+  int32_t Temperature = (int32_t)(int16_t)(Wire.read() << 8 | Wire.read());
+  int32_t gyroX = (int32_t)(int16_t)(Wire.read() << 8 | Wire.read());
+  int32_t gyroY = (int32_t)(int16_t)(Wire.read() << 8 | Wire.read());
+  int32_t gyroZ = (int32_t)(int16_t)(Wire.read() << 8 | Wire.read());
+  int now_time = millis();
+  if(data_index == 0){
+    sprintf(data_buffer, "[");
+    data_index++;
   }
-  int now_time[3];
-  char acc = 'X';
-  //much better to send as one packet!!!!!!! MUST BE CHANGED. code on raspberry will work with only the time as given by the X acceleration. 
-  for (int i = 0; i < 3; i++) {
-    char msg[100];
-    now_time[i] = millis();
-    int use_time = last_time[i] == 0 && cal_counter >= num_cal ? 0 : now_time[i] - last_time[i];
-    sprintf(msg, "5/%c acceleration: %d.|%d", acc + i, acc_data[i], use_time);
-    client.publish(topic, msg); //5 inicates normal data transfer message
+  if(data_index == size_data_buffer-150){
+    data_buffer = (char*)realloc(data_buffer, size_data_buffer*2); //should make this safer. in case it fails. ANY IDEAS TO WHOEVER READS THIS? it's about when realloc returns null (same issue for calloc).\
+    size_data_buffer *= 2;
   }
-  cal_counter++;
-  if(cal_counter < 20){
-      delay(500);
-  }
-
-  last_time = copy_arr(now_time, last_time, 3);
-
+  data_index += sprintf((data_buffer+data_index), "{\"AcX\": %d, \"AcY\": %d, \"AcZ\": %d, \"GyroX\": %d, \"GyroY\": %d, \"GyroZ\": %d, \"Time\": %d},", (int)AcX, AcY, AcZ, gyroX, gyroY, gyroZ, (now_time - begin_time));
 }
 
-  int* copy_arr(int* src, int* dst, int arr_length){
-    for(int i = 0; i < arr_length; i++){
-      dst[i] = src[i];
+int determine_i2c_addr() {
+  //to determine which i2c address the accelerometer has
+  for (int addr = 1; addr < 127; addr++) {
+    Wire.beginTransmission(addr);
+    if (!Wire.endTransmission()) {
+      char msg[5];
+      sprintf(msg, "%d", addr);
+      client.publish(topic, msg);
     }
-    return dst;
   }
+}
+
+int* copy_arr(int* src, int* dst, int arr_length) {
+  for (int i = 0; i < arr_length; i++) {
+    dst[i] = src[i];
+  }
+  return dst;
+}
